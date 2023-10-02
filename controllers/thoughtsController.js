245780c2c3758218c74a3f5c62b87a9fc83529ca
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-02 18:31:26 -0400

    updated controllers

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,79 @@
+const { Thought, Reaction } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
+                .select('-__v');
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with that ID' });
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ name: req.params.thoughtId });
+            res.status(200).json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async createReaction(req, res) {
+        try {
+            const reaction = await Reaction.create(req.body);
+            res.json(reaction);
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    },
+
+    async deleteReaction(req, res) {
+        try {
+            const reaction = await Reaction.findOneAndDelete({ name: req.params.reactionId });
+            res.status(200).json(reaction);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+};
